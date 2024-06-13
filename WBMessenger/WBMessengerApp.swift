1690e30ea8e2805f04eb7e325c3d36a59c41a561
noychus <157799428+noychus@users.noreply.github.com>
2024-06-13 07:48:17 +0300

    Initial Commit

---
+++ WBMessenger/WBMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBMessengerApp.swift
+//  WBMessenger
+//
+//  Created by NOY on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
